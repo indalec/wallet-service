86aa3b5d2f468e4e97fc963ac50b66c811b8a1cf
indaleci <indaleci4@gmail.com>
2026-08-24 02:36:15 +0200

    Added TransferService

---
+++ src/main/java/com/indalec/walletservice/service/TransferService.java
@@ -0,0 +1,75 @@
+package com.indalec.walletservice.service;
+
+import com.indalec.walletservice.model.Transfer;
+import com.indalec.walletservice.model.TransferStatus;
+import com.indalec.walletservice.model.Wallet;
+import com.indalec.walletservice.repository.TransferRepository;
+import com.indalec.walletservice.repository.WalletRepository;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.math.BigDecimal;
+import java.time.LocalDateTime;
+import java.util.UUID;
+
+@Service
+public class TransferService {
+
+    private final WalletRepository walletRepository;
+    private final TransferRepository transferRepository;
+
+    public TransferService(
+            WalletRepository walletRepository,
+            TransferRepository transferRepository
+    ) {
+        this.walletRepository = walletRepository;
+        this.transferRepository = transferRepository;
+    }
+
+    //With Transactional the transfer will either complete entirely or have no effect at all.
+    @Transactional
+    public Transfer transfer(
+            UUID sourceWalletId,
+            UUID destinationWalletId,
+            BigDecimal amount,
+            String currency
+    ) {
+
+        Wallet source = walletRepository.findById(sourceWalletId)
+                .orElseThrow(() -> new RuntimeException("Source wallet not found"));
+
+        Wallet destination = walletRepository.findById(destinationWalletId)
+                .orElseThrow(() -> new RuntimeException("Destination wallet not found"));
+
+        if (sourceWalletId.equals(destinationWalletId)) {
+            throw new RuntimeException("A wallet cannot transfer money to itself");
+        }
+
+        if (amount.compareTo(BigDecimal.ZERO) <= 0) {
+            throw new RuntimeException("Transfer amount must be greater than zero");
+        }
+
+        if (!source.getCurrency().equals(currency)
+                || !destination.getCurrency().equals(currency)) {
+            throw new RuntimeException("Currency mismatch");
+        }
+
+        if (source.getBalance().compareTo(amount) < 0) {
+            throw new RuntimeException("Insufficient funds");
+        }
+
+        source.setBalance(source.getBalance().subtract(amount));
+        destination.setBalance(destination.getBalance().add(amount));
+
+        Transfer transfer = new Transfer(
+                source,
+                destination,
+                amount,
+                currency,
+                LocalDateTime.now(),
+                TransferStatus.COMPLETED
+        );
+
+        return transferRepository.save(transfer);
+    }
+}
