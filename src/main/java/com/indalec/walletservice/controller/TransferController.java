36988db06f3eb0400afb2519fde00d8371b526f6
indaleci <indaleci4@gmail.com>
2026-08-24 12:33:04 +0200

    Added Transfer Controller and CreateTransferController, transfer test succesful

---
+++ src/main/java/com/indalec/walletservice/controller/TransferController.java
@@ -0,0 +1,34 @@
+package com.indalec.walletservice.controller;
+
+import com.indalec.walletservice.dto.CreateTransferRequest;
+import com.indalec.walletservice.model.Transfer;
+import com.indalec.walletservice.service.TransferService;
+import jakarta.validation.Valid;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/transfers")
+public class TransferController {
+
+    private final TransferService transferService;
+
+    public TransferController(TransferService transferService) {
+        this.transferService = transferService;
+    }
+
+    @PostMapping
+    public Transfer createTransfer(
+            // Valid checks the validation rules from CreateTransferRequest,RequestBody converts the JSON from the HTTP to an Object
+            @Valid @RequestBody CreateTransferRequest request
+    ) {
+        return transferService.transfer(
+                request.sourceWalletId(),
+                request.destinationWalletId(),
+                request.amount(),
+                request.currency()
+        );
+    }
+}
---
+++ src/main/java/com/indalec/walletservice/dto/CreateTransferRequest.java
@@ -0,0 +1,24 @@
+package com.indalec.walletservice.dto;
+
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Positive;
+
+import java.math.BigDecimal;
+import java.util.UUID;
+
+public record CreateTransferRequest(
+
+        @NotNull
+        UUID sourceWalletId,
+
+        @NotNull
+        UUID destinationWalletId,
+
+        @NotNull
+        @Positive
+        BigDecimal amount,
+
+        @NotNull
+        String currency
+) {
+}
